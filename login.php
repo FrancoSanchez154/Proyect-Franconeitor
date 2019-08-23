07bf03f79c112b349b0b45978373fb74cfc4533f
Franco Sanchez <francosanchezkpo@gmail.com>
2019-08-23 12:07:41 -0300

    aber

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
 	session_start();
 
+	if(empty($_POST['entrar']))
+	{
 	//conexion a la BASE
 	mysql_connect ("localhost", "root","15709595hola");
 	mysql_select_db ("cuentas_registradas");
@@ -10,12 +12,20 @@
 	$res=mysql_query($sql);
 	$num=mysql_num_rows($res);
 
+	//SE FIJA SI HAY ALGUN REGISTRO
 	if($num=0)
 	{
+		//CREAR SESION CON LA ID DEL USUARIO
 		$row=mysql_fetch_array($res);
 		 
 		$_SESSION['osu_id']=$row['id_usuario'];
+
+		//Redireccion al login
+		header("Location:login.php");
+	
+		}
 	}
+
 ?>
 
 
