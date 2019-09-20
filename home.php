19f8a7a8b2f0133b9899eb83e1024b50a252e018
Franco Sanchez <francosanchezkpo@gmail.com>
2019-09-20 19:48:42 -0300

    funciones y diseño del home (pude mostrar el div de otro archivo dentro de un div en el home)

--- home.php
+++ home.php
@@ -12,50 +12,68 @@ session_start();
 
     </head>
 
-    <body background="images/fondo.jpg">
-    	<center><input type="button" id="comenzar" value="Comenzar" style="text-align:center;background-color: coral ;font-style: italic ;font-size:30px ; height: 50px;width: 150px"></center>
+    <body>
+
+        <button class="boton_personalizado" type="submit"  id="comenzar">COMENZAR</button>
+
+        <!-- <center><input type="button" id="comenzar" value="Comenzar" style="text-align:center;background-color: coral ;font-style: italic ;font-size:30px ; height: 50px;width: 150px">
+    	</center> -->
+
         <!-- <div id="front" style=""> -->
-            <!-- <img src="images/0QKVdhG.jpg"> -->
+        <!-- <img src="images/0QKVdhG.jpg"> -->
         <!-- </div> -->
-        
+
         <div id="templates">
-        	<div id="primera">
-        		<img src="images/template1.jpg">
-        	</div>
-        	<div id="segunda">
-        		<img src="images/template2.png">
-        	</div>
-        	<div id="tercera">
-        		<img src="images/template3.jpg">
-        	</div>
-        	<div id="cuarta">
-        		<img src="images/template4.jpg">
-        	</div>
+            <div id="primera">
+                <img src="images/template1.jpg">
+            </div>
+            <div id="segunda">
+                <img src="images/template2.png">
+            </div>
+            <div id="tercera">
+                <img src="images/template3.jpg">
+            </div>
+            <div id="cuarta">
+                <img src="images/template4.jpg">
+            </div>
 
         </div>
+        <div id="contenido"></div>
 
         <script type="text/javascript" src="vendor/jquery/jquery.js"></script>
 
         <script type="text/javascript">
             $(document).ready(function() {
                 $('#comenzar').on("click", function() {
-                    $('#front').animate({
-                        opacity: 0,
-                    }, 1000);
-
+                    // $('#front').animate({
+                        // opacity: 0,
+                    // }, 1000);
                     $('#templates').animate({
-                    	top: 50,
-                    	opacity: 1
-                    	}, 	1500 );
-
+                        top: 40,
+                        opacity: 1
+                    }, 1500);
                     $('#comenzar').animate({
-                    	opacity:0,
-                    	}, 1000);
-
-
-
-                    });
+                        opacity: 0
+                    }, 1000);
+                    $('#comenzar').hide(1);
+                });
             });
+            // Aca dentro esta cuando apretar el boton y te aparecen los diseños
+        </script>
+        <script type="text/javascript">
+        	$(document).ready(function(){
+        		$('#primera').on("click", function(){
+        			$('#contenido').load('opcion1.php #opcion1');
+         			$('#contenido').animate({
+        				opacity: 1
+        			}, 1500);
+
+        			$('#templates').animate({
+        				opacity: 0.5
+        			}, 1000);
+        		});
+        	});
+
         </script>
 
     </body>
---
+++ opcion1.php
@@ -0,0 +1,17 @@
+
+<div id="opcion1">
+
+<h1>la puta madre mostra esto</h1>
+</div>
+<div id="opcion2">
+
+<h1>22222222222</h1>
+</div>
+<div id="opcion3">
+
+<h1>333333333333333333333</h1>
+</div>
+<div id="opcion4">
+
+<h1>444444444444</h1>
+</div>
