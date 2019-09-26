21fd694e7218ebebbc73e654288042bc1e456000
Franco Sanchez <francosanchezkpo@gmail.com>
2019-09-26 11:32:18 -0300

    ndndndnd

--- opcion1.php
+++ opcion1.php
@@ -10,9 +10,35 @@
 			<label> TITULO</label>
 			<input type="text" name="titulo" required>
 			<br />
+			<label> Cantidad de paginas</label>
+			<input type="number" name="num_pag" min="1" required>
+			<br />
+			<label> Medidas</label>
+			<input type="text" name="medidas" placeholder="example: 13,34 × 20,96" required>cm
+			<br />
+			<label> Tapa</label>
+			<select name="tapa">
+				<option value="Tapa blanda">Tapa blanda</option>
+				<option value="Tapa dura">Tapa dura</option>
+
+			</select>
+			<br />
+			<label> Editorial</label>
+			<input type="text" name="editorial">
+			<br />
+			<label> Reseña</label>
+			<input type="text" name="reseña" required>
+			<br />
+			<label>Fecha de publicacion</label>
+			<input type="date" name="fecha_publicacion" required>
+			<br />
+
 			<label>Seleccione las imagenes</label>
 			<input type="file" name="imagen" required>
 			<br />
+
+
+
 			<input type="submit" name="publicar" value="publicar">
 
 
--- validarpublicaciones.php
+++ validarpublicaciones.php
@@ -2,6 +2,14 @@
 include 'conexion.php';
 
 $titulo=$_REQUEST['titulo'];
+$num_pag=$_REQUEST['num_pag'];
+$medidas=$_REQUEST['medidas'];
+$tapa=$_REQUEST['tapa'];
+$editorial=$_REQUEST['editorial'];
+$reseña=$_REQUEST['reseña'];
+$fecha_publicacion=$_REQUEST['fecha_publicacion'];
+
+
 
 $nombreimg=$_FILES['imagen']['name'];
 $archivo=$_FILES['imagen']['tmp_name'];
@@ -10,7 +18,8 @@ $ruta="images";
 $ruta=$ruta."/".$nombreimg;
 
 move_uploaded_file($archivo, $ruta);
-$query="INSERT INTO post VALUES('', '".$titulo."', '".$ruta."') ";
+$query="INSERT INTO post VALUES('', '".$titulo."','".$num_pag."','".$medidas."','".$tapa."','".$editorial."','".$reseña."','".$fecha_publicacion."','".$ruta."') ";
+
 $consulta = mysqli_query($con, $query);//ERA ESTOOOOO <--------------------
 
 if ($query) {
