694bcd220d398757b3de3fb91e061806e81924fe
Franco Sanchez <francosanchezkpo@gmail.com>
2019-10-09 18:45:29 -0300

    Deje mas compacto el codigo, y saque una animacion que era innecesaria

--- home.php
+++ home.php
@@ -15,61 +15,29 @@ session_start();
 
         <button class="boton_personalizado" type="submit" id="comenzar">COMENZAR</button>
 
-        <!-- <center><input type="button" id="comenzar" value="Comenzar" style="text-align:center;background-color: coral ;font-style: italic ;font-size:30px ; height: 50px;width: 150px">
-    	</center> -->
-
-        <!-- <div id="front" style=""> -->
-        <!-- <img src="images/0QKVdhG.jpg"> -->
-        <!-- </div> -->
-
-        <div id="templates">
-            <div id="primera" name="imagenes">
-                <img src="images/template1.jpg">
-            </div>
-            <!--    <div id="segunda" name="video">
-                <img src="images/template2.png">
-            </div>
-            <div id="tercera">
-                <img src="images/template3.jpg">
-            </div>
-            <div id="cuarta">
-                <img src="images/template4.jpg">
-            </div> -->
-
         </div>
 
         <div id="contenido"></div>
 
         <script type="text/javascript" src="vendor/jquery/jquery.js"></script>
 
+
         <script type="text/javascript">
-            $(document).ready(function() {
-                $('#comenzar').on("click", function() {
-                    // $('#front').animate({
-                    // opacity: 0,
-                    // }, 1000);
-                    $('#templates').animate({
-                        top: 40,
-                        opacity: 1
-                    }, 1500);
+            $(document).ready(function(){
+                $('#comenzar').on("click", function(){
                     $('#comenzar').animate({
                         opacity: 0
                     }, 1000);
                     $('#comenzar').hide(1);
                 });
             });
-            // Aca dentro esta cuando apretar el boton y te aparecen los diseños
         </script>
         <script type="text/javascript">
             $("#contenido").hide()
             $(document).ready(function() {
-                $('#primera').on("click", function() {
+                $('#comenzar').on("click", function() {
                     $('#contenido').load('crearpost.php #crearpost');
-                    $('#contenido').fadeIn();
-
-                    /*$('#templates').animate({
-                    	opacity: 0.5
-                    }, 1000);*/
+                    $('#contenido').fadeIn(1200);
                 });
             });
         </script>
